ef9932a11f09a52738fe9fd37b48b69e52828fd2
Brian Henning <henninb@gmail.com>
2024-03-26 08:47:36 -0500

    wip

---
+++ client-side.js
@@ -0,0 +1,40 @@
+var p = document.getElementsByTagName('script')[0],
+s = document.createElement('script');
+s.async = 1;
+s.src = '/ARWFVlUf/init.js';
+p.parentNode.insertBefore(s, p);
+
+    window._PXARWFVlUf = {
+        challenge: {
+         context: {
+          headerText: 'Are you a real person?',
+          logoImgSrc: "https://henninb.github.io/human-challenge/brian-logo.png",
+          headerFontSize: '22px',
+          headerColor: "#f8f8f2",
+          messageText: 'Help us keep your account safe by completing the below action.',
+          messageFontSize: '16px',
+          messageColor: '#f8f8f2',
+          messagePadding: '40px',
+         },
+         translation: {
+          "default": {
+           btn: 'PRESS AND HOLD',
+           textSize: 15,
+          },
+         },
+         view: {
+          fillColor: "#44475a",
+          backgroundColor: "#282a36",
+          borderColor: "#6272a4",
+          borderRadius: 50,
+          preset: 1,
+          borderWidth: 4,
+          textColor: "#bd93f9",
+          textSize: 10,
+          fontWeight: 'bold',
+         }
+        }
+       };
+
+
+
