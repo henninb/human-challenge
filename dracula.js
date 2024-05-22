2bc91f5efd4ff0eef5bb4ee199122a31b303ad90
Brian Henning <henninb@gmail.com>
2024-05-22 09:28:08 -0500

    wip

--- dracula.js
+++ dracula.js
@@ -1,55 +1,32 @@
 // window['_' + window._pxAppId] = {
 window._PXjJ0cYtn9 = {
- challenge: {
-  context: {
-   headerText: 'Are you a real person?',
-   logoImgSrc: "https://henninb.github.io/human-challenge/brian-logo.png",
-   headerFontSize: '22px',
-   headerColor: "#f8f8f2",
-   messageText: 'Help us keep your account safe by completing the below action.',
-   messageFontSize: '16px',
-   messageColor: '#f8f8f2',
-   messagePadding: '40px',
-  },
-  translation: {
-   "default": {
-    btn: 'PRESS AND HOLD',
-    textSize: 15,
-   },
-  },
-  view: {
-   fillColor: "#44475a",
-   backgroundColor: "#282a36",
-   borderColor: "#6272a4",
-   borderRadius: 50,
-   preset: 1,
-   borderWidth: 4,
-   textColor: "#bd93f9",
-   textSize: 10,
-   fontWeight: 'bold',
+  challenge: {
+    context: {
+      headerText: 'Are you a real person?',
+      logoImgSrc: "https://henninb.github.io/human-challenge/brian-logo.png",
+      headerFontSize: '22px',
+      headerColor: "#373a40",  // Dark color for text on white background
+      messageText: 'Help us keep your account safe by completing the below action.',
+      messageFontSize: '16px',
+      messageColor: "#373a40",  // Dark color for text on white background
+      messagePadding: '40px',
+    },
+    translation: {
+      "default": {
+        btn: 'PRESS AND HOLD',
+        textSize: 15,
+      },
+    },
+    view: {
+      fillColor: "#f8f8f2",  // Light color for elements
+      backgroundColor: "#ffffff",  // White background
+      borderColor: "#e0e0e0",  // Light border for contrast
+      borderRadius: 50,
+      preset: 1,
+      borderWidth: 4,
+      textColor: "#373a40",  // Dark color for text on light background
+      textSize: 10,
+      fontWeight: 'bold',
+    }
   }
- }
-};
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-async function loadCss() {
-  let captchaContainer;
-  // let captchaWrapper;
-
-  // while (!captchaWrapper) {
-  //   captchaWrapper = document.querySelector("#px-captcha-wrapper > div");
-  //   await sleep(100);
-  // }
-  // captchaWrapper.style.backgroundColor = '#282a36';
-
-  while (!captchaContainer) {
-    captchaContainer = document.querySelector('div.px-captcha-container');
-    await sleep(100);
-  }
-  captchaContainer.style.borderBottom = 'solid 12px #F3BC2A';
-}
-
-loadCss();
